c5f442f443ea31daefe879e2f72cf9eaa5819ea4
Charlie1991 <juancarlosnbcc@gmail.com>
2023-10-25 10:27:00 -0600

    edited

---
+++ 2023-10-23-for-loop-1-to-100/04-loops/Fuzzbuzz-function/app.js
@@ -0,0 +1,12 @@
+const isDiviz = () => {};
+for (let i = 1; i <= 10; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("BuzzFizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
---
+++ 2023-10-23-for-loop-1-to-100/loops/app.js
@@ -0,0 +1,23 @@
+// 3 coding structures that change the order of operations in an app
+
+// 1. Conditionals
+const isTiredOfMondays = true;
+if (isTireOfMondays) {
+  console.log("Take a nap");
+} else {
+  console.log("Get to work");
+}
+
+// 2. Functions
+const greet = function () {
+  console.log("Hi, ${name}");
+};
+greet("Tony");
+greet("Chris");
+greet("Ayan");
+
+//3. Loops
+
+for (let i = 1; i <= 100; i++) {
+  console;
+}
