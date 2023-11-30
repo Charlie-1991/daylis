740edf4a9b903676c507d5c6c295cb387aac9f0c
Charlie1991 <juancarlosnbcc@gmail.com>
2023-11-30 10:55:19 -0700

    added

---
+++ automated-testing/__tests__/1.superSimple.js
@@ -0,0 +1,18 @@
+
+const sauces = {
+  ketchup: 'red'
+  mustard: 'yellow'
+  relish: 'green'
+}
+
+
+describe("sauces", () => {
+  describe("ketchup color", () => {
+    it(succes.ketchup===,'' () => {
+      expect(true).toBeTruthy();
+    });
+    test("must c", () => {
+      expect(true).toBeTruthy();
+    });
+  });
+});
